83c757aa01f1a108531815e2e790e4a2fe7cc03c
anweshaswain-mcd <anwesha.swain@capgemini.com>
2024-01-30 15:33:47 +0530

    Deleting a student

--- studentadminportal-ui/src/app/students/view-students/view-students.component.ts
+++ studentadminportal-ui/src/app/students/view-students/view-students.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from '../../models/ui-models/student.model';
 import { GenderService } from '../../services/gender.service';
 import { Gender } from '../../models/ui-models/gender.model';
@@ -36,7 +36,7 @@ export class ViewStudentsComponent implements OnInit {
   genderList: Gender[] = [];
 
   constructor(private readonly studentService: StudentService, private readonly route: ActivatedRoute,
-    private readonly genderService: GenderService, private snackBar: MatSnackBar) {}
+    private readonly genderService: GenderService, private snackBar: MatSnackBar, private router: Router) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -71,6 +71,23 @@ export class ViewStudentsComponent implements OnInit {
       },
       (errorResponse) => {
         //log it
+      }
+    );
+  }
+
+  onDelete(): void {
+    //call student service to delete the student
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse) => {
+        this.snackBar.open('Student deleted successfully', undefined, { duration: 2000 });
+
+        setTimeout(() => {
+          this.router.navigateByUrl('students');
+        }, 2000);
+      },
+      (errorResponse) => {
+        //log
       }
     );
   }
